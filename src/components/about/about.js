7020b292bb876f3915d06c70b7241022a8181620
Andrew McMillion <amcmillion3@gmail.com>
2022-03-09 08:49:42 -0500

    Add nav and about

---
+++ src/components/about/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>We have been serving the citizens of Hyrule since before the Castle was built. We have everything an adventurer could need.</p>
+        </div>
+    )
+}
