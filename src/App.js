146e010a340b4a317cd461749352a9aafc8cf707
Andrew McMillion <amcmillion3@gmail.com>
2022-03-15 16:32:15 -0400

    Import useState Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import { productData as data } from "./assets/product-data";
